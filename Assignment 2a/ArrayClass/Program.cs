ef820c353bc37bc130b8f050f64891698d2e23bd
Arindam Gohain <sunnyraj24@gmail.com>
2017-09-25 17:37:10 +0530

    Second Commit

--- Assignment 2a/ArrayClass/Program.cs
+++ Assignment 2a/ArrayClass/Program.cs
@@ -6,7 +6,20 @@ namespace ArrayClass
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello");
+           int ctr = 1; 
+           Console.WriteLine("Please enter the sie of the array:");
+           int size = int.Parse(Console.ReadLine());
+           int[] arr = new int[size];
+           for(int i =0; i<size; i++)
+           {
+               arr[i] = ctr;
+               Console.WriteLine(arr[i]);
+               if(i == 10)
+               {
+                   Console.WriteLine("Reached 10");
+               }
+               ctr++; 
+           }            
         }
     }
 }
